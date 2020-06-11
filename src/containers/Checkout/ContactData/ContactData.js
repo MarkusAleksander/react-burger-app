faf0dc9a4d69cdf579aea610f0bba2b5e90b3adf
MarkusAleksander <markholden1989@googlemail.com>
2020-06-11 13:50:24 +0100

    course form update

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -18,6 +18,11 @@ class ContactData extends Component {
                     placeholder: "Your Name",
                 },
                 value: "",
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
             },
             street: {
                 elementType: "input",
@@ -26,6 +31,11 @@ class ContactData extends Component {
                     placeholder: "Street",
                 },
                 value: "",
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
             },
             zipCode: {
                 elementType: "input",
@@ -34,6 +44,13 @@ class ContactData extends Component {
                     placeholder: "Zip Code",
                 },
                 value: "",
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5,
+                },
+                valid: false,
+                touched: false,
             },
             country: {
                 elementType: "input",
@@ -42,6 +59,11 @@ class ContactData extends Component {
                     placeholder: "Your Country",
                 },
                 value: "",
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
             },
             email: {
                 elementType: "input",
@@ -50,6 +72,11 @@ class ContactData extends Component {
                     placeholder: "Your Email",
                 },
                 value: "",
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
             },
             deliveryMethod: {
                 elementType: "select",
@@ -66,8 +93,12 @@ class ContactData extends Component {
                     ],
                 },
                 value: "",
+                validation: {},
+                valid: true,
+                touched: true,
             },
         },
+        formIsValid: false,
         loading: false,
     };
 
@@ -105,6 +136,28 @@ class ContactData extends Component {
             });
     };
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (!rules) {
+            return isValid;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== "" && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm,
@@ -113,8 +166,22 @@ class ContactData extends Component {
         updatedFormElement.value = event.target.value;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
 
+        updatedFormElement.valid = this.checkValidity(
+            updatedFormElement.value,
+            updatedFormElement.validation
+        );
+
+        updatedFormElement.touched = true;
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid =
+                updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+
         this.setState({
             orderForm: updatedOrderForm,
+            formIsValid: formIsValid,
         });
     };
 
@@ -136,12 +203,17 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) =>
                             this.inputChangedHandler(event, formElement.id)
                         }
                     />
                 ))}
-                <Button btnType="Success">ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>
+                    ORDER
+                </Button>
             </form>
         );
 
